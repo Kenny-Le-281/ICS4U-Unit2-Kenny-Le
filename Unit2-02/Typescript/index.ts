41e2b8ea4b6fdf347dd9a3c2b021f461aaccd6ce
Kenny Le <kenny.le@mths.ca>
2024-03-22 14:04:40 +0000

    ts

---
+++ Unit2-02/Typescript/index.ts
@@ -0,0 +1,55 @@
+/**
+* This program uses the MrCoxallStack
+*
+* By:      Kenny Le
+* Version: 1.0
+* Since:   2024-03-22
+*/
+
+import { MrCoxallStack } from './MrCoxallStack'
+
+// Uses MrCoxallStack
+const fruitStack = new MrCoxallStack()
+fruitStack.push("apple")
+fruitStack.push("banana")
+fruitStack.push("cherry")
+
+// Shows the fruit stack items
+console.log("Fruit items: ", fruitStack.showStack())
+
+const topFruit = fruitStack.pop()
+
+// Shows the popped item and new list
+console.log("Pop fruit item: ", topFruit)
+console.log("Fruit items: ", fruitStack.showStack())
+
+const colorStack = new MrCoxallStack()
+colorStack.push("red")
+colorStack.push("green")
+colorStack.push("blue")
+
+// Shows the color stack items
+console.log("Color items: ", colorStack.showStack())
+
+const topColor = colorStack.pop()
+
+// Shows the popped item and new list
+console.log("Pop color item: ", topColor)
+console.log("Color items: ", colorStack.showStack())
+
+const dogStack = new MrCoxallStack()
+dogStack.push("poodle")
+dogStack.push("retriever")
+dogStack.push("beagle")
+
+// Shows the dog stack items
+console.log("Dog items: ", dogStack.showStack())
+
+const topDog = dogStack.pop()
+
+// Shows the popped item and new list
+console.log("Pop dog item: ", topDog)
+console.log("Dog items: ", dogStack.showStack())
+
+// Show the program as done
+console.log('\nDone.')
