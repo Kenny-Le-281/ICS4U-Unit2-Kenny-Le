7983f504ec0205797e859b0b6f258a9f1eb0c447
Kenny Le <99355017+Kenny-Le-281@users.noreply.github.com>
2024-03-26 09:52:42 -0400

    ts

---
+++ Unit2-03/Typescript/fruit.ts
@@ -0,0 +1,41 @@
+/**
+* This program uses the MrCoxallStack
+*
+* By:      Kenny Le
+* Version: 1.0
+* Since:   2024-03-22
+*/
+
+import { MrCoxallStack } from './MrCoxallStack'
+
+// Checksize of stack
+const fruitStackSize = fruitStack.size()
+console.log("The size of the stack is: ", fruitStackSize)
+
+// Check boolean is empty
+const booleanCheck = fruitStack.isEmpty()
+console.log("Is the stack empty? ", booleanCheck)
+
+// Print fruit items
+const fruitStack = new MrCoxallStack()
+fruitStack.push("apple")
+fruitStack.push("banana")
+fruitStack.push("cherry")
+console.log("Fruit items: ", fruitStack.showStack())
+
+// Shows the popped item and new list
+const topFruit = fruitStack.pop()
+console.log("Pop fruit item: ", topFruit)
+console.log("Fruit items: ", fruitStack.showStack())
+console.log()
+
+// Checksize of stack
+const fruitStackSize = fruitStack.size()
+console.log("The size of the stack is: ", fruitStackSize)
+
+// Check boolean is empty
+const booleanCheck = fruitStack.isEmpty()
+console.log("Is the stack empty? ", booleanCheck)
+
+// Show the program as done
+console.log('\nDone.')
