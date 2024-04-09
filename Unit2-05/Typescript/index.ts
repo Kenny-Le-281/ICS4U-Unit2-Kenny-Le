e866ee8c625fe634870745b282e249c6e6e0b918
Kenny Le <99355017+Kenny-Le-281@users.noreply.github.com>
2024-04-08 23:07:47 -0400

    ts

--- Unit2-05/Typescript/index.ts
+++ Unit2-05/Typescript/index.ts
@@ -1,18 +1,18 @@
 /**
 * This is the main code that runs
  * with the Vehicle Class.
- * By:      Kenny Le
+ * By:      Mr Coxall
  * Version: 1.0
- * Since:   2024-04-01
+ * Since:   2020-01-01
  */
 
-import {Vehicle} from './Vehicle.ts'
+import { Vehicle } from './Vehicle.ts'
 
 console.log('Created Ford focus')
 const fordFocus = new Vehicle('white', 'B40-C4RR0', 4, 284)
 
 console.log('Status:')
-fordFocus.status()
+console.log(fordFocus.status)
 
 console.log('Accelerating, 10 of power for 10 sec.')
 fordFocus.accelerate(10, 10)
@@ -29,14 +29,14 @@ console.log('Created a Ferrari')
 const ferrari = new Vehicle('red', 'VHD-35HU', 2, 320)
 
 console.log('Status:')
-ferrari.status()
+console.log(ferrari.status)
 
 console.log('Accelerating, 100 of power for 4 sec.')
 ferrari.accelerate(100, 4)
 console.log(`New speed: ${ferrari.speed}`)
 
-console.log('Breaking, 10 of power for 10 sec.')
-ferrari.break(10, 10)
+console.log('Brakeing, 10 of power for 10 sec.')
+ferrari.brake(10, 10)
 console.log(`New speed: ${ferrari.speed}`)
 
 console.log('Changing color:')
@@ -50,14 +50,14 @@ console.log('Created a Citroen Ami')
 const citroenAmi = new Vehicle('blue', 'FS-695-DQ', 2, 45)
 
 console.log('Status:')
-citroenAmi.status()
+console.log(citroenAmi.status)
 
 console.log('Accelerating, 5 of power for 10 sec.')
 citroenAmi.accelerate(5, 10)
 console.log(`New speed: ${citroenAmi.speed}`)
 
-console.log('Breaking, 2 of power for 10 sec.')
-citroenAmi.break(2, 10)
+console.log('Brakeing, 2 of power for 10 sec.')
+citroenAmi.brake(2, 10)
 console.log(`New speed: ${citroenAmi.speed}`)
 
 console.log('Changing color:')
