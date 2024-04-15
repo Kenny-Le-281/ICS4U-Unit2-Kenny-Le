049e1dd95bff07d6a589cb88a4c6cc67aa0ee10c
Kenny Le <99355017+Kenny-Le-281@users.noreply.github.com>
2024-04-15 00:25:05 -0400

    lint

--- Unit2-06/Typescript/index.ts
+++ Unit2-06/Typescript/index.ts
@@ -29,14 +29,14 @@ while (counter < 4) {
   if (boeing.getSpeed() > 5000) {
   // If the speed of the boeing object is greater than NUMBER_5000,
   // double the speed of the biplane object
-  biplane.setSpeed(biplane.getSpeed() * 2)
+    biplane.setSpeed(biplane.getSpeed() * 2)
   } else {
     // If the speed of the boeing object is not greater than NUMBER_5000,
     // accelerate the boeing object again
     boeing.accelerate()
   }
 
-    counter++; // Increment the counter
+  counter++; // Increment the counter
 }
 
 boeing.setSpeed(424)
