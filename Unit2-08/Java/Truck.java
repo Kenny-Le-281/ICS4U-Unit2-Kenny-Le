5cf578f774695d4e95c37e7a2b8f47e95d1e81f1
Kenny Le <99355017+Kenny-Le-281@users.noreply.github.com>
2024-04-21 22:03:16 -0400

    java

---
+++ Unit2-08/Java/Truck.java
@@ -0,0 +1,84 @@
+/*
+* This class creates a truck using methods from vehicles
+*
+* @author  Kenny Le
+* @version 1.0
+* @since   2024-04-09
+*/
+
+/**
+ * This is the class.
+*/
+public class Truck extends Vehicle {
+    /**
+     * Represents the number of wheels on the vehicle.
+     * This constant stores the total number of wheels that the vehicle has.
+    */
+    private static final int NUMBER_OF_WHEELS = 4;
+    
+    /**
+     * Variable for the class.
+     *
+     * @param licensePlateNumber the license plate number on the vehicle
+    */
+    private String licensePlateNumber = "";
+
+    /**
+     * Creates the base truck.
+     *
+     * @param initialColour the base colour
+     * @param initialPlateNumber the base license plate number
+     * @param initialMaxSpeed the maximum speed of the vehicle
+    */
+    public Truck(
+            String initialColour,
+            double initialMaxSpeed,
+            String initialPlateNumber
+    ) {
+        super(initialColour, initialMaxSpeed, NUMBER_OF_WHEELS);
+        this.licensePlateNumber = initialPlateNumber;
+    }
+
+    /**
+     * Gets the license plate number of the vehicle.
+     *
+     * @return the current licensePlateNumber
+    */
+    public String getLicensePlateNumber() {
+        return this.licensePlateNumber;
+    }
+
+    /**
+     * Creates a string about the status of the vehicle.
+     *
+     * @return the string about the vehicle's status
+    */
+    public String getStatus() {
+        final String lineBreak = "\n";
+        String status = " -> Speed: " + this.getSpeed() + lineBreak;
+        status += " -> MaxSpeed: " + this.getMaxSpeed() + lineBreak;
+        status += " -> Color: " + this.getColour() + lineBreak;
+        status += " -> License Plate: " + this.licensePlateNumber + lineBreak;
+        status += " -> # of Wheels: " + this.getWheels() + lineBreak;
+
+        return status;
+    }
+
+    /**
+     * Sets the license plate of the vehicle.
+     *
+     * @param newPlateNumber of the vehicle.
+    */
+    public void setLicensePlateNumber(String newPlateNumber) {
+        this.licensePlateNumber = newPlateNumber;
+    }
+
+    /**
+     * Provides air the the wheels of the truck, which changes speed.
+     *
+     * @param airPressure applied to the truck
+    */
+    public void applyAir(double airPressure) {
+        this.setSpeed(this.getSpeed() - airPressure / 2);
+    }
+}
