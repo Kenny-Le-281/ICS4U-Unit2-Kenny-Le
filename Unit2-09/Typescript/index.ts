57ae39e3156a303297eb777114c1c9d3fa9e4b06
Kenny Le <99355017+Kenny-Le-281@users.noreply.github.com>
2024-04-23 00:05:24 -0400

    ts

--- Unit2-09/Typescript/index.ts
+++ Unit2-09/Typescript/index.ts
@@ -1,88 +1,47 @@
-/**
-* This class creates a vehicle
+
+/*
+* This program is a vehicle class.
 *
-* By:      Kenny Le
-* Version: 1.0
-* Since:   2024-04-12
+* @author  Kenny Le
+* @version 1.0
+* @since   2024-04-12
 */
 
-export class Vehicle {
-	// Class variables
+import { Truck } from "./Truck"
+import { Bike } from "./Bike"
 
-	// Vehicle colour
-	protected colour: string
-	// Current speed of the vehicle
-	protected speed: number
-	// Maximum speed of the vehicle
-	protected maxSpeed: number
-	// Total number of wheels
-	protected wheels: number
+const bmx = new Bike('Red', 40)
 
-	// Creates the base vehicle
-	public constructor(initialColour: string, initialMaxSpeed: number, initialWheels: number) {
-		this.colour = initialColour
-		this.speed = 0
-		this.maxSpeed = initialMaxSpeed
-		this.wheels = initialWheels
-	}
+console.log('Created Bmx bike.\nStatus:')
+console.log(bmx.status)
 
-	// Gets the colour of the vehicle
-	public get colour(): string {
-		return this.colour
-	}
+console.log('\nSet the cadence to 10')
+bmx.cadence = 10
+bmx.speed = 20
+console.log(bmx.status)
 
-	// Gets the current speed of the vehicle
-	public get speed(): number {
-		return this.speed
-	}
+console.log('\nAccelerate by 15:')
+bmx.accelerate(15)
+console.log(bmx.status)
 
-	// Gets the maximum speed of the vehicle
-	public get maxSpeed(): number {
-		return this.maxSpeed
-	}
+console.log('\nRinging bell.')
+bmx.ringBell()
 
-	// Gets the number of wheels
-	public get wheels() {
-		return this.wheels
-	}
+const bigTruck = new Truck('Grey', 200, 'HGC-3456F')
 
-	// Creates a string about the status of the car
-	public get status(): string {
-		let statusString = 'Status:\n'
-		statusString = ` -> Speed: ${this.speed}\n`
-		statusString += ` -> MaxSpeed: ${this.maxSpeed}\n`
-		statusString += ` -> Color: ${this.colour}\n`
-		statusString += ` -> Number of Wheels: ${this.wheels}`
+console.log('Created a Truck.\nStatus:')
+console.log(bigTruck.status)
 
-		return statusString
-	}
+console.log('\nAccelerating, 10 of power for ten seconds:')
+bigTruck.accelerate(10, 10)
+console.log('New speed: ' + bigTruck.speed)
 
-	// Sets the colour of the vehicle
-	public set colour(newColour: string) {
-		this.colour = newColour
-	}
+console.log('\nBreaking, 10 of power for 10 sec.')
+bigTruck.accelerate(10, 10)
+console.log('New speed: ' + bigTruck.speed)
 
-	// Accelerates the car, changing the speed
-  public accelerate(accelerationPower: number, accelerationTime: number) {
-    if (accelerationPower > 0 && accelerationTime > 0) {
-      this.speed = (accelerationPower * accelerationTime) + this.speed
-      if (this.speed > this.maxSpeed) {
-        this.speed = this.maxSpeed
-      }
-    } else {
-      console.log("Acceleration power and time must be positive numbers.")
-    }
-  }
+console.log('\nApplyed air pressure of 10:')
+bigTruck.applyAir(10)
+console.log('New speed: ' + bigTruck.speed)
 
-  // Brakes the car, changing the speed
-  public brake(brakePower: number, brakeTime: number) {
-    if (brakePower > 0 && brakeTime > 0) {
-      this.speed = this.speed - (brakePower * brakeTime)
-      if (this.speed < 0) {
-        this.speed = 0
-      }
-    } else {
-      console.log("Brake power and time must be positive numbers.")
-    }
-  }
-}
+console.log('\nDone.')
